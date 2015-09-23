bd3f277d09d6ebf95676856c832532e306da10ce
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-23 11:51:00 +0200

    type annotation - examples given with the read function

---
+++ ch03/type_annotation.hs
@@ -0,0 +1,9 @@
+changeToInt = read "5" :: Int
+
+changeToFloat = read "5" :: Float
+
+mutipleWithFloat = ( read "5" :: Float ) * 4
+
+changeToIntList = read "[1,2,3,4]" :: [ Int ]
+
+changeToTuple = read "(3,'a')" :: ( Int , Char )
