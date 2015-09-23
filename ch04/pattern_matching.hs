c33950dab295df72c913a8a4a82d69e08530a6f8
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-23 23:08:37 +0200

    vectors

--- ch04/pattern_matching.hs
+++ ch04/pattern_matching.hs
@@ -11,3 +11,14 @@ sayMe 5 = "Fem!"
 sayMe 6 = "Seks!"
 sayMe 7 = lucky 7
 sayMe x = "Ikke mellem 0 og 8"
+
+addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
+--addVectors a b = (fst a + fst b, snd a + snd b)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a,b,c) -> a
+first (x, _, _) = x
+second :: (a,b,c) -> b
+second (_, x, _) = x
+third :: (a,b,c) -> c
+third (_, _, x) = x
