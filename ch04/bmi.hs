2a95a553bdb1a3c0f152e2321148ab806daebee8
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-24 17:04:25 +0200

    more local variables in where

--- ch04/bmi.hs
+++ ch04/bmi.hs
@@ -1,8 +1,11 @@
-bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell :: (Show a, RealFloat a) => a -> a -> String
 bmiTell weight height
-  | bmi <= 18.5 = "You're underweight, you emo, you!"
-  | bmi <= 25   = "You're supposedly normal. Pffft, I bet you're ugly!"
-  | bmi <= 30   = "You're fat! Lose some weight, fatty!"
-  | otherwise   = "You're a whale, congratulations!"
+  | bmi <= skinny = "You're underweight, you emo, you! bmi: " ++ show bmi
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly! bmi: " ++ show bmi
+  | bmi <= fat    = "You're fat! Lose some weight, fatty! bmi: " ++ show bmi
+  | otherwise     = "You're a whale, congratulations! bmi: " ++ show bmi
   where
     bmi = weight / height ^ 2
+    skinny = 18.5
+    normal = 25
+    fat    = 30
