11480e95c5e85d3985e30830e627bfcf3061270e
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-24 12:54:27 +0200

    sum function

--- ch04/pattern_matching.hs
+++ ch04/pattern_matching.hs
@@ -54,3 +54,7 @@ tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " 
 length' :: (Num b) => [a] -> b
 length' [] = 0 -- pattern matching (edge condition)
 length' (_:xs) = 1 + length' xs -- recursive
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
