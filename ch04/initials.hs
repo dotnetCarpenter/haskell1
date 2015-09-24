7651abadaed4747f4cd35cad68dbe94ff862bd16
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-24 17:19:40 +0200

    pattern matching directly in the functions's parameters

--- ch04/initials.hs
+++ ch04/initials.hs
@@ -2,3 +2,6 @@ initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
     where (f:_) = firstname
           (l:_) = lastname
+
+initials' :: String -> String -> String
+initials' (f:_) (l:_) = [f] ++ ". " ++ [l] ++ "."
