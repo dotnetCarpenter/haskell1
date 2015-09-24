69141e90ddbadfaa6a21f46df2c689b3bc194158
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-24 12:40:40 +0200

    a bit more advanced list pattern matches - tell and length'

--- ch04/pattern_matching.hs
+++ ch04/pattern_matching.hs
@@ -26,3 +26,31 @@ third (_, _, x) = x
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, stupid!"
 head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+-- could also be written as list literal [x]
+tell (x:[]) = "The list has one element: " ++ show x
+-- could also be written as list literal [x,y]
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+-- This is similar to the factorial function we wrote earlier. First we defined
+-- the result of a known input — the empty list. This is also known as the edge
+-- condition. Then in the second pattern we take the list apart by splitting it
+-- into a head and a tail. We say that the length is equal to 1 plus the length of
+-- the tail. We use _ to match the head because we don’t actually care what it
+-- is. Also note that we’ve taken care of all possible patterns of a list. The first
+-- pattern matches an empty list and the second one matches anything that isn’t
+-- an empty list.
+-- Let’s see what happens if we call length’ on "ham". First, it will check if it’s an
+-- empty list. Because it isn’t, it falls through to the second pattern. It matches on
+-- the second pattern and there it says that the length is 1 + length’ "am", because
+-- we broke it into a head and a tail and discarded the head. O-kay. The length’ of
+-- "am" is, similarly, 1 + length’ "m". So right now we have 1 + (1 + length’ "m").
+-- length’ "m" is 1 + length’ "" (could also be written as 1 + length’ []). And we’ve
+-- defined length’ [] to be 0. So in the end we have 1 + (1 + (1 + 0)).
+
+length' :: (Num b) => [a] -> b
+length' [] = 0 -- pattern matching (edge condition)
+length' (_:xs) = 1 + length' xs -- recursive
