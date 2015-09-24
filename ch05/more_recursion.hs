b50b447e324069ddb6a627f75eb03c09f0dfd301
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-24 21:22:14 +0200

    take function

--- ch05/more_recursion.hs
+++ ch05/more_recursion.hs
@@ -2,3 +2,11 @@ replicate' :: (Num i, Ord i) => i -> a -> [a]
 replicate' n x
     | n <= 0    = []
     | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
+-- TODO: implement a duplicate function that finds duplicates in a list
