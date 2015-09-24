aa7c2e3b4f843f78d7b29d7f4793316e32511bd1
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-24 21:25:24 +0200

    reverse function

--- ch05/more_recursion.hs
+++ ch05/more_recursion.hs
@@ -9,4 +9,8 @@ take' n _
 take' _ []     = []
 take' n (x:xs) = x : take' (n-1) xs
 
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
 -- TODO: implement a duplicate function that finds duplicates in a list
