57393ea3faea2bfabd798354beac155a1f802d6f
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-10-01 14:25:29 +0200

    fixed previous note

--- ch06/curried.hs
+++ ch06/curried.hs
@@ -1,5 +1,8 @@
 -- max 4 5 == (max 4) 5
--- every function in haskell takes only one parameter os all functions in haskell are curried functions
+-- every function in haskell takes only one parameter so all multiple parameter functions in haskell are curried functions
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * x
 
 -- calling a function that takes two parameters with one parameter will return a function
 -- that take the second parameter. Here is an example with "compare"
