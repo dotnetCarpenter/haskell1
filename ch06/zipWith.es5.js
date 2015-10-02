e8db8e000cfbc63365b2554724d2193a3037655e
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-10-02 23:56:11 +0200

    more examples of zipWith to confirm interop with haskell's zipWith

--- ch06/zipWith.es5.js
+++ ch06/zipWith.es5.js
@@ -4,6 +4,10 @@ var add = function add(a, b) {
   return a + b;
 };
 
+var multiply = function multiply(a, b) {
+  return a * b;
+};
+
 var zipWith = function zipWith(f, xs, ys) {
   if (xs.length == 0) return [];
   if (ys.length == 0) return [];
@@ -12,6 +16,15 @@ var zipWith = function zipWith(f, xs, ys) {
   return [f(x, y)].concat(zipWith(f, xs, ys));
 };
 
+var replicate = function replicate(i, a) {
+  if (i === 0) return [];
+  return [a].concat(replicate(--i, a));
+};
+
 console.log(add(5, 5)); // -> 10
 console.log(zipWith(add, [4, 2, 5, 6], [2, 6, 2, 3])); // -> [ 6, 8, 7, 9 ]
-console.log(zipWith(add, [4, 2, 5, 6, 3, 65, 2, 1], [2, 6, 2, 3])); // -> [ 6, 8, 7, 9 ]
+console.log(zipWith(add, [4, 2, 5, 6, 3, 65, 2, 1], [2, 6, 2, 3])); // -> [6, 8, 7, 9]
+console.log(zipWith(Math.max, [6, 3, 2, 1], [7, 3, 1, 5])); // -> [7, 3, 2, 5]
+console.log(zipWith(add, ["foo", "bar", "baz"], ["fighters", "hoppers", "aldrin"])); // -> ["foofighters","barhoppers","bazaldrin"]
+console.log(replicate(5, 2)); // -> [2,2,2,2,2]
+console.log(zipWith(multiply, replicate(5, 2), [1, 2, 3, 4, 5])); // -> [2 ,4 ,6 ,8 ,10]
--- ch06/zipWith.js
+++ ch06/zipWith.js
@@ -2,6 +2,8 @@
 
 var add = (a, b) => a + b
 
+var multiply = (a, b) => a * b
+
 var zipWith = (f, xs, ys) => {
   if(xs.length == 0) return []
   if(ys.length == 0) return []
@@ -10,6 +12,15 @@ var zipWith = (f, xs, ys) => {
   return [f(x, y)].concat(zipWith(f, xs, ys))
 }
 
+var replicate = (i, a) => {
+  if(i === 0) return []
+  return [a].concat(replicate(--i, a))
+}
+
 console.log(add(5, 5)) // -> 10
 console.log( zipWith( add, [4,2,5,6], [2,6,2,3]) ) // -> [ 6, 8, 7, 9 ]
-console.log( zipWith( add, [4,2,5,6,3,65,2,1], [2,6,2,3]) ) // -> [ 6, 8, 7, 9 ]
+console.log( zipWith( add, [4,2,5,6,3,65,2,1], [2,6,2,3]) ) // -> [6, 8, 7, 9]
+console.log( zipWith( Math.max, [6 ,3 ,2 ,1], [7 ,3 ,1 ,5]) ) // -> [7, 3, 2, 5]
+console.log( zipWith( add, ["foo","bar","baz"], ["fighters","hoppers","aldrin"]) ) // -> ["foofighters","barhoppers","bazaldrin"]
+console.log( replicate(5,2) ) // -> [2,2,2,2,2]
+console.log( zipWith( multiply, replicate(5, 2), [1,1,1,1,1] ) ) // -> [2 ,4 ,6 ,8 ,10]
