a7e4315a6e518e691cbf8177eb039b16b5ef8d7d
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-01-04 18:24:52 +0200

    added documentation about curried infix functions

--- ch06/curried.hs
+++ ch06/curried.hs
@@ -9,7 +9,10 @@ multThree x y z = x * y * x
 compareWithHundred :: (Num a, Ord a) => a -> Ordering
 compareWithHundred = compare 100 -- compare takes two parameter, e.i. compare 100 101
 
--- infix partial applied function
+-- infix partial applied function:
+-- To section an infix function, simply surround it with parentheses and only supply a parameter
+-- on one side. That creates a function that takes one parameter and then applies it to the side that's
+-- missing an operand. divideByTen 200 is equivalent to doing 200 / 10, as is doing (/10) 20
 divideByTen :: (Floating a) => a -> a
 divideByTen = (/10)
 
